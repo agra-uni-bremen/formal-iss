59bab034afe6516346ec097dea1d595ee8ab19f9
Sören Tempel <tempel@uni-bremen.de>
2023-02-21 12:42:19 +0100

    Initial code generation work

--- src/Generator.hs
+++ src/Generator.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
 module Generator (runExpression, generate) where
 
 import Language.C
@@ -10,6 +12,10 @@ import LibRISCV.Decoder (InstructionType)
 import LibRISCV.Spec.Operations
 import qualified LibRISCV.Spec.Expr as E
 
+import qualified Interface as IF
+
+type Evaluator = (E.Expr CExpr -> CExpr)
+
 -- TODO: Treat inner value as unsigned
 -- TODO: Don't create NodePos explicitly
 runExpression :: E.Expr CExpr -> CExpr
@@ -17,14 +23,20 @@ runExpression (E.FromImm e)  = e
 runExpression (E.FromInt v)  = let cint = CInteger (fromIntegral v) HexRepr noFlags in
                                 CConst (CIntConst cint $ mkNodeInfoOnlyPos nopos)
 runExpression (E.AddU e1 e2) = CBinary CAddOp (runExpression e1) (runExpression e2) $ mkNodeInfoOnlyPos nopos
+runExpression (E.AddS e1 e2) = CBinary CAddOp (runExpression e1) (runExpression e2) $ mkNodeInfoOnlyPos nopos
 runExpression (E.Eq e1 e2)   = CBinary CEqOp  (runExpression e1) (runExpression e2) $ mkNodeInfoOnlyPos nopos
 runExpression _            = error "not implemented"
 
 ------------------------------------------------------------------------
 
-generate :: InstructionType -> Eff '[Operations (E.Expr CExpr)] w -> [CStat]
-generate _ req = snd $ run (runWriter (reinterpret gen req))
+generate :: InstructionType -> Evaluator -> Eff '[Operations CExpr] w -> [CStat]
+generate _ evalE req = snd $ run (runWriter (reinterpret gen req))
   where
-    gen :: Operations (E.Expr CExpr) ~> Eff '[Writer [CStat]]
-    gen (ReadRegister _) = tell [CBreak $ mkNodeInfoOnlyPos nopos] >> pure (E.FromInt 0)
+    gen :: Operations CExpr ~> Eff '[Writer [CStat]]
+    gen (GetRD  instr)          = pure $ IF.instrRD instr
+    gen (GetRS1 instr)          = pure $ IF.instrRS1 instr
+    gen (GetRS2 instr)          = pure $ IF.instrRS2 instr
+    gen (ReadRegister idx)      = pure $ IF.readReg idx
+    gen (WriteRegister idx val) = tell [CExpr (Just $ IF.writeReg idx (evalE val)) $ mkNodeInfoOnlyPos nopos]
+    -- gen (ReadRegister _) = tell [CBreak $ mkNodeInfoOnlyPos nopos] >> pure (E.FromInt 0)
     gen _ = error "not implemented"
--- src/Interface.hs
+++ src/Interface.hs
@@ -1,5 +1,5 @@
 -- This module defines the C API required to be implemented for the ISS.
-module Interface (readRegFunc, writeRegFunc) where
+module Interface where
 
 import Language.C
 
@@ -12,8 +12,8 @@ names = newNameSupply
 --
 --  uint32_t read_register(unsigned idx);
 --
-readRegFunc :: CExpr -> CExpr
-readRegFunc idx = CCall (CVar funcName $ mkNodeInfoOnlyPos nopos) [idx] $ mkNodeInfoOnlyPos nopos
+readReg :: CExpr -> CExpr
+readReg idx = CCall (CVar funcName $ mkNodeInfoOnlyPos nopos) [idx] $ mkNodeInfoOnlyPos nopos
   where
     funcName :: Ident
     funcName = mkIdent nopos "read_register" $ names !! 0
@@ -22,8 +22,40 @@ readRegFunc idx = CCall (CVar funcName $ mkNodeInfoOnlyPos nopos) [idx] $ mkNode
 --
 --  void write_register(unsigned idx, uint32_t val)
 --
-writeRegFunc :: CExpr -> CExpr -> CExpr
-writeRegFunc idx val = CCall (CVar funcName $ mkNodeInfoOnlyPos nopos) [idx, val] $ mkNodeInfoOnlyPos nopos
+writeReg :: CExpr -> CExpr -> CExpr
+writeReg idx val = CCall (CVar funcName $ mkNodeInfoOnlyPos nopos) [idx, val] $ mkNodeInfoOnlyPos nopos
   where
     funcName :: Ident
     funcName = mkIdent nopos "write_register" $ names !! 1
+
+------------------------------------------------------------------------
+
+-- Interface contract:
+--
+--  uint32_t instr_rs1(void *instr);
+--
+instrRS1 :: CExpr -> CExpr
+instrRS1 instr = CCall (CVar funcName $ mkNodeInfoOnlyPos nopos) [instr] $ mkNodeInfoOnlyPos nopos
+  where
+    funcName :: Ident
+    funcName = mkIdent nopos "instr_rs1" $ names !! 2
+
+-- Interface contract:
+--
+--  uint32_t instr_rs2(void *instr);
+--
+instrRS2 :: CExpr -> CExpr
+instrRS2 instr = CCall (CVar funcName $ mkNodeInfoOnlyPos nopos) [instr] $ mkNodeInfoOnlyPos nopos
+  where
+    funcName :: Ident
+    funcName = mkIdent nopos "instr_rs2" $ names !! 3
+
+-- Interface contract:
+--
+--  uint32_t instr_rd(void *instr);
+--
+instrRD :: CExpr -> CExpr
+instrRD instr = CCall (CVar funcName $ mkNodeInfoOnlyPos nopos) [instr] $ mkNodeInfoOnlyPos nopos
+  where
+    funcName :: Ident
+    funcName = mkIdent nopos "instr_rd" $ names !! 4
