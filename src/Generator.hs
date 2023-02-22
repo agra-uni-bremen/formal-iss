3f519de87eb84766fa37a686f9883d7537b8b148
Sören Tempel <tempel@uni-bremen.de>
2023-02-22 13:24:14 +0100

    Generate: Call buildInstruction'' in generate directly

--- src/Generator.hs
+++ src/Generator.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
-module Generator (runExpression, generate) where
+module Generator (generate) where
 
 import Language.C
 
@@ -9,30 +9,29 @@ import Control.Monad.Freer
 import Control.Monad.Freer.Writer
 
 import LibRISCV.Decoder (InstructionType)
+import LibRISCV.Spec.AST (buildInstruction'')
 import LibRISCV.Spec.Operations
 import qualified LibRISCV.Spec.Expr as E
 
+import Executor
 import qualified Interface as IF
-
-type Evaluator = (E.Expr CExpr -> CExpr)
 
 -- TODO: Treat inner value as unsigned
 -- TODO: Don't create NodePos explicitly
-runExpression :: E.Expr CExpr -> CExpr
-runExpression (E.FromImm e)  = e
-runExpression (E.FromInt v)  = let cint = CInteger (fromIntegral v) HexRepr noFlags in
+evalE :: E.Expr CExpr -> CExpr
+evalE (E.FromImm e)  = e
+evalE (E.FromInt v)  = let cint = CInteger (fromIntegral v) HexRepr noFlags in
                                 CConst (CIntConst cint $ mkNodeInfoOnlyPos nopos)
-runExpression (E.AddU e1 e2) = CBinary CAddOp (runExpression e1) (runExpression e2) $ mkNodeInfoOnlyPos nopos
-runExpression (E.AddS e1 e2) = CBinary CAddOp (runExpression e1) (runExpression e2) $ mkNodeInfoOnlyPos nopos
-runExpression (E.Eq e1 e2)   = CBinary CEqOp  (runExpression e1) (runExpression e2) $ mkNodeInfoOnlyPos nopos
-runExpression _            = error "not implemented"
+evalE (E.AddU e1 e2) = CBinary CAddOp (evalE e1) (evalE e2) $ mkNodeInfoOnlyPos nopos
+evalE (E.AddS e1 e2) = CBinary CAddOp (evalE e1) (evalE e2) $ mkNodeInfoOnlyPos nopos
+evalE (E.Eq e1 e2)   = CBinary CEqOp  (evalE e1) (evalE e2) $ mkNodeInfoOnlyPos nopos
+evalE _            = error "not implemented"
 
 ------------------------------------------------------------------------
 
--- TODO: Call buildInstruction'' for the instruction type.
 -- TODO: Emit a C function definition for the given instruction type.
-generate :: InstructionType -> Evaluator -> Eff '[Operations CExpr] w -> [CStat]
-generate _ evalE req = snd $ run (runWriter (reinterpret gen req))
+generate' :: Eff '[Operations CExpr] w -> [CStat]
+generate' req = snd $ run (runWriter (reinterpret gen req))
   where
     gen :: Operations CExpr ~> Eff '[Writer [CStat]]
     gen (GetRD  instr)          = pure $ IF.instrRD instr
@@ -42,3 +41,8 @@ generate _ evalE req = snd $ run (runWriter (reinterpret gen req))
     gen (WriteRegister idx val) = tell [CExpr (Just $ IF.writeReg idx (evalE val)) $ mkNodeInfoOnlyPos nopos]
     -- gen (ReadRegister _) = tell [CBreak $ mkNodeInfoOnlyPos nopos] >> pure (E.FromInt 0)
     gen _ = error "not implemented"
+
+generate :: InstructionType -> [CStat]
+generate inst = generate' cflow
+  where
+    cflow = buildInstruction'' @CExpr (CVar instrIdent (mkNodeInfoOnlyPos nopos)) inst
--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,14 @@
 module Main where
 
+import LibRISCV (RegIdx(..))
+import LibRISCV.Decoder (InstructionType(..))
+
 import Language.C
-import LibRISCV.Spec.Expr
 import Generator
 
 main :: IO ()
 main = do
-    let result = runExpression $ (Eq (AddU (FromInt 42) (FromInt 23)) (FromInt 1337))
-    putStrLn $ "Expr: " ++ show (pretty result)
+    let inst = ADD Zero Zero Zero
+    let stats = head $ generate inst
+
+    putStrLn $ show (pretty stats)
