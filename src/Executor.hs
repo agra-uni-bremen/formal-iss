3b9f9d388523911cef85a3d6b0be032bc4f820c4
Sören Tempel <tempel@uni-bremen.de>
2023-02-22 19:51:29 +0100

    Start working on Executor abstraction
    Generates function definitions on a per-instruction bassi.

---
+++ src/Executor.hs
@@ -0,0 +1,42 @@
+module Executor (instrIdent, makeExecutor) where
+
+import Language.C
+import Interface (names)
+
+instrIdent :: Ident
+instrIdent = mkIdent nopos "instr" (names !! 5)
+
+instrArg :: CDecl
+instrArg = CDecl
+    [(CTypeSpec $ CVoidType (mkNodeInfoOnlyPos nopos))]
+    [((Just instrArg'), Nothing, Nothing)]
+    (mkNodeInfoOnlyPos nopos)
+  where
+    instrArg' :: CDeclr
+    instrArg' = CDeclr (Just instrIdent) [CPtrDeclr [] (mkNodeInfoOnlyPos nopos)] Nothing [] (mkNodeInfoOnlyPos nopos)
+
+-- regIndent :: Ident
+-- regIndent = mkIdent nopos "regFile" (names !! 6)
+
+------------------------------------------------------------------------
+
+noReturn :: CDeclSpec
+noReturn = CTypeSpec $ CVoidType (mkNodeInfoOnlyPos nopos)
+
+mkFuncDeclr :: Ident -> [CDecl] -> CDeclr
+mkFuncDeclr ident args = CDeclr (Just ident) [mkFuncDeclr' args] Nothing [] (mkNodeInfoOnlyPos nopos)
+    where
+        mkFuncDeclr' :: [CDecl] -> CDerivedDeclr
+        mkFuncDeclr' args' = CFunDeclr (Right (args', False)) [] (mkNodeInfoOnlyPos nopos)
+
+makeBlock :: CStat -> CStat
+makeBlock stat = CCompound [] [CBlockStmt stat] (mkNodeInfoOnlyPos nopos)
+
+
+makeExecutor :: Ident -> CStat -> CFunDef
+makeExecutor ident statement = CFunDef
+                                    [noReturn]
+                                    (mkFuncDeclr ident [instrArg])
+                                    []
+                                    (makeBlock statement)
+                                    (mkNodeInfoOnlyPos nopos)
