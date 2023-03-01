21cba46ed4aa63657c27e9c0a388b4324405c220
Sören Tempel <tempel@uni-bremen.de>
2023-03-01 22:15:12 +0100

    Bindings: Move function names to own list

--- src/Bindings.hs
+++ src/Bindings.hs
@@ -4,24 +4,26 @@ import Language.C
 import Data.Maybe (fromJust)
 import qualified Data.Map as M
 
+-- Statically known function names required to be provided as an Interface.
+funcNames :: [String]
+funcNames = [
+                "read_register",  -- Read register interface function
+                "write_register", -- Write register interface function
+
+                "instr_rs1",      -- Interface function for RS1 access
+                "instr_rs2",      -- Interface function for RS2 access
+                "instr_rd"        -- Interface function for RD access
+            ]
+
 -- Store Ident here, not Name
 type Bindings = M.Map String Ident
 
 mkBinding :: String -> Name -> (String, Ident)
 mkBinding s name = (s, mkIdent nopos s name)
 
--- TODO: Fold
 mkBindings :: [Name] -> Bindings
-mkBindings names = M.fromList $ snd $ foldl (\(n, acc) s -> (tail n, (mkBinding s $ head n) : acc)) (names, []) ids
-  where
-    ids = [
-            "read_register",  -- Read register interface function
-            "write_register", -- Write register interface function
-
-            "instr_rs1",      -- Interface function for RS1 access
-            "instr_rs2",      -- Interface function for RS2 access
-            "instr_rd"        -- Interface function for RD access
-          ]
+mkBindings names = M.fromList $ snd $
+    foldl (\(n, acc) s -> (tail n, (mkBinding s $ head n) : acc)) (names, []) funcNames
 
 getIdent :: String -> Bindings -> Ident
 getIdent name bindings = fromJust (M.lookup name bindings)
